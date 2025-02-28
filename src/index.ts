bb4b46a06c206711223dcac8af86d620fefbcc1f
0xSachinK <0xsachink@gmail.com>
2025-02-28 20:01:46 +0530

    fix published package; fix index.ts file etxtension

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,39 @@
+// Group related imports together
+import {
+  PaymentPlatform,
+  PaymentPlatformConfig,
+  PaymentPlatformType,
+  paymentPlatforms,
+  ProofExtractedParameters
+} from './types';
+
+import {
+  Currency,
+  CurrencyType,
+  currencies,
+  currencyInfo,
+  getCurrencyInfoFromHash,
+  getCurrencyInfoFromCountryCode,
+  isSupportedCurrency
+} from './utils/currency';
+
+// Import platform configs
 import { cashappConfig } from './platforms/cashapp/platformConfig';
+import { mercadoPagoConfig } from './platforms/mercado_pago/platformConfig';
 import { revolutConfig } from './platforms/revolut/platformConfig';
 import { venmoConfig } from './platforms/venmo/platformConfig';
 import { wiseConfig } from './platforms/wise/platformConfig';
-import { 
-  PaymentPlatform, 
-  PaymentPlatformConfig, 
-  PaymentPlatformType, 
-  paymentPlatforms, 
-  ProofExtractedParameters 
-} from './types';
 
-import { 
-  Currency, 
-  CurrencyType, 
-  currencies, 
-  currencyInfo, 
-  getCurrencyInfoFromHash, 
-  getCurrencyInfoFromCountryCode, 
-  isSupportedCurrency 
-} from './utils/currency';
-
+// Create platform info map
 const paymentPlatformInfo: Record<string, PaymentPlatformConfig> = {
   [PaymentPlatform.CASHAPP]: cashappConfig,
   [PaymentPlatform.REVOLUT]: revolutConfig,
   [PaymentPlatform.VENMO]: venmoConfig,
   [PaymentPlatform.WISE]: wiseConfig,
+  [PaymentPlatform.MERCADO_PAGO]: mercadoPagoConfig,
 };
 
+// Export everything in organized groups
 export {
   // Currency exports
   Currency,
@@ -36,11 +43,11 @@ export {
   getCurrencyInfoFromHash,
   getCurrencyInfoFromCountryCode,
   isSupportedCurrency,
-  
+
   // Payment platform exports
   PaymentPlatform,
   PaymentPlatformType,
   paymentPlatforms,
   paymentPlatformInfo,
   ProofExtractedParameters,
-};
+}; 
