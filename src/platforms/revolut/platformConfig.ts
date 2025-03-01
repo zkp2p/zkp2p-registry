75ac6b98e1c63f3891bd123efa35cbaccc8927d5
0xSachinK <0xsachink@gmail.com>
2025-03-01 18:26:27 +0530

    patch revolut payee id key

--- src/platforms/revolut/platformConfig.ts
+++ src/platforms/revolut/platformConfig.ts
@@ -96,7 +96,7 @@ export const revolutConfig: PaymentPlatformConfig = {
     payeeDetailInputPlaceholder: "Enter your Revtag",
     payeeDetailInputHelperText: "This is your Revtag. Make sure you have set your Revtag to be publicly discoverable.",
     payeeDetailValidationFailureMessage: "Make sure you have set your Revtag to be publicly discoverable and there are no typos.",
-    payeeIdKey: 'revtag',
+    payeeIdKey: 'revolutUsername',
     getDepositData,
     validateDepositData
   },
